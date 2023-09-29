595fc61d7e1013567be8ba81d6b80810cc1f4d2e
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-09-29 19:39:21 +0530

    Partially complete register function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 
 const loginUser = asyncHandler(async (req, res) => {
@@ -22,7 +23,31 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register user' });
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 8000;
+import connectDB from './config/db.js';
 import {
     notFound,
     errorHandler
@@ -9,7 +10,14 @@ import {
 import userRoutes from "./routes/userRoutes.js";
 
 
+connectDB();
+
+
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/api/users', userRoutes);
